50e8171db258508fa4a0cf4998611c89907f0ab9
Sparshlohana <sparsh.lohana@gmail.com>
2023-02-09 15:08:42 +0530

    contact me

--- src/components/Contact-me/ContactMe.jsx
+++ src/components/Contact-me/ContactMe.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactMeHeading from "../ContactMeHeading/ContactMeHeading";
+import ContactMeIcons from "../ContactMeIcons/ContactMeIcons";
 import "./contactMe.css";
 
 const ContactMe = () => {
   return (
     <div className="contactMe">
       <ContactMeHeading />
+      <ContactMeIcons />
     </div>
   );
 };
---
+++ src/components/ContactMeIcons/ContactMeIcons.jsx
@@ -0,0 +1,95 @@
+import "./contactMeIcons.css";
+
+const ContactMeIcons = () => {
+  return (
+    <div className="contactMeIconContainer">
+      <div className="iconContainer">
+        <a
+          href="https://api.whatsapp.com/send?phone=8154000250&text=Heyy%20Sparsh..%20Nice%20to%20meet%20you!!!"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/479px-WhatsApp.svg.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+
+      <div className="iconContainer">
+        <a
+          href="https://www.instagram.com/its_.sparshiii/"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+      <div className="iconContainer">
+        <a
+          href="https://www.linkedin.com/in/sparsh-r-lohana-845457242/"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+      <div className="iconContainer">
+        <a
+          href="https://github.com/Sparshlohana"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+      <div className="iconContainer">
+        <a
+          href="https://twitter.com/Sparsh_8304"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+      <div className="iconContainer">
+        <a
+          href="https://www.facebook.com/sparshr.lohana"
+          className="contactMeIconsAnchor"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/124/124010.png"
+            alt="WPIcon"
+            className="contactMeIcons"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMeIcons;
